b4a54e5bf4a5dc4c8801c313ff51e282ed11f295
gootoomoon <gootoomoon@gmail.com>
2013-07-11 03:29:27 +0000

    fix digital.c test
    Signed-off-by: gootoomoon <gootoomoon@gmail.com>

--- cb_test/c_test/digital.c
+++ cb_test/c_test/digital.c
@@ -1,13 +1,16 @@
 /**
  * softPwm test 
  * author:gootoomoon 
- * gcc -o test c_digital.c -lwiringPi -lpthread
+ * gcc -o test digital.c -lwiringPi -lpthread
  */
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 
 #include <wiringPi.h>
+#define OUTPUT 	1
+#define HIGH 	1
+#define LOW 	0
 
 int pin = 1;
 
@@ -18,11 +21,12 @@ int main()
 		fprintf(stdout, "wiring lib init error:%s", strerror(errno));	
 		return 1;
 	}
+	pinMode(pin,OUTPUT);
 
 	for(;;){
-		digitalWrite(pin,1);
+		digitalWrite(pin,HIGH);
 		delay(1000);
-		digitalWrite(pin,0);
+		digitalWrite(pin,LOW);
 		delay(1000);
 	}
 }
